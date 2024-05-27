afac34d0762282bcd5b995753fc9e1ba3daa6ce6
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-27 07:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+let submit = document.getElementById("btn");
+
+submit.addEventListener("click",()=>{
+	setTimeout(()=>{
+		let out =  document.getElementById("output");
+		out.innerText = text.value;
+	},delay.value)
+})
